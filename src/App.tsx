53637ee7b6117352d75163214ceb46510b867540
nanni4287 <nanni.lee@aceenter.com>
2022-05-03 18:31:25 +0900

    컴포넌트 하나씩만 보여주게 하는 거 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Main from './components/main';
 import Second from './components/second';
@@ -6,51 +6,33 @@ import Third from './components/third';
 import Nav from "./components/nav";
 
 function App() {
-    const pageHeight = window.innerHeight;
-    const outerDivRef:any = useRef<null | HTMLDivElement>();
+    const [isFirstDown, setIsFirstDown] = useState("first");
 
-    const fullPageScroll = () => {
-        window.addEventListener('wheel', function(event){
-
-            setTimeout(() => {
-                if(event.deltaY < 0){
-                    if(window.scrollY <= pageHeight){
-                        window.scrollTo({
-                            top : 0,
-                            behavior : "smooth",
-                        })
-                    }else {
-                        window.scrollTo({
-                            top : pageHeight,
-                            behavior : "smooth"
-                        })
-                    }
-
-                }else if(event.deltaY > 0){
-                    if(window.scrollY >= 0 && window.scrollY < pageHeight){
-                        window.scrollTo({
-                            top : pageHeight,
-                            behavior : "smooth"
-                        })
-                    }else if(window.scrollY >= pageHeight && window.scrollY < pageHeight * 2){
-                        window.scrollTo({
-                            top : pageHeight * 2,
-                            behavior : "smooth"
-                        })
-                    }
-                }
-            }, 200);
-
-        });
+    const slidingPage = () => {
+        window.addEventListener('wheel', (event) => {
+            if(event.deltaY > 0){
+                setIsFirstDown("second");
+            }else{
+                setIsFirstDown("first");
+            }
+        })
     }
 
-
-    return (
-        <div className="App" onWheel={fullPageScroll} ref={outerDivRef}>
+    if(isFirstDown === "second"){
+        return <div className="App" onWheel={slidingPage}>
+            <Nav />
+            <Second />
+        </div>
+    }else if(isFirstDown === "first"){
+        return <div className="App" onWheel={slidingPage}>
             <Nav />
             <Main />
-            <Second />
-            <Third />
+        </div>
+    }
+
+    return (
+        <div className="App" onWheel={slidingPage}>
+            <Nav />
         </div>
     );
 }
