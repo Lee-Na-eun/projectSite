ac4a259e22a70703c3c9ab4530db9a71da108bcd
nanni4287 <nanni.lee@aceenter.com>
2022-05-03 12:27:25 +0900

    fullpage slide 버블링 해결 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './App.css';
 import Main from './components/main';
 import Second from './components/second';
@@ -8,7 +8,9 @@ import Nav from "./components/nav";
 let ing = false;
 
 function App() {
-    const [nowFullPage, setNowFullPage] = useState(0);
+    const pageHeight = window.innerHeight;
+    const outerDivRef:any = useRef<null | HTMLDivElement>();
+
     const fullPageScroll = () => {
         window.addEventListener('wheel', function(event){
             if(ing){
@@ -18,29 +20,50 @@ function App() {
             ing = true;
 
             if(event.deltaY < 0){
+                console.log("scrollY : ", window.scrollY);
+                console.log("pageHeight : ", pageHeight)
+                if(window.scrollY <= pageHeight){
+                    window.scrollTo({
+                        top : 0,
+                        behavior : "smooth"
+                    })
+                }else {
+                    window.scrollTo({
+                        top : pageHeight,
+                        behavior : "smooth"
+                    })
+                }
                 setTimeout(() => {
                     console.log('scrolling up');
                     ing = false;
-                }, 800);
+                }, 200);
 
             }else if(event.deltaY > 0){
+                if(window.scrollY >= 0 && window.scrollY < pageHeight){
+                    window.scrollTo({
+                        top : pageHeight,
+                        behavior : "smooth"
+                    })
+                }else if(window.scrollY >= pageHeight && window.scrollY < pageHeight * 2){
+                    window.scrollTo({
+                        top : pageHeight * 2,
+                        behavior : "smooth"
+                    })
+                }
                 setTimeout(() => {
-                    setNowFullPage(1);
                     console.log('scrolling down');
                     ing = false;
-                }, 800);
+                }, 200);
             }
         })
     }
 
-
+    console.log(pageHeight);
 
     return (
-        <div className="App" onWheel={fullPageScroll}>
+        <div className="App" onWheel={fullPageScroll} ref={outerDivRef}>
             <Nav />
-            <div id={"test"}>
-                <Main />
-            </div>
+            <Main />
             <Second />
             <Third />
         </div>
